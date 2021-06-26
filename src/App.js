8c303a6624560d70575d014a6a4203a85a62dd45
Fabian Osorio Iriarte <fabiani@uninorte.edu.co>
2021-06-26 17:11:47 -0500

    Added 404 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
 import BlogDetail from './BlogDetail';
+import NotFound from './NotFound';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -19,6 +20,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetail />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
