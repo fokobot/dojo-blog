9b249886197364f3701b2bf78287d047ba798442
Fabian Osorio Iriarte <fabiani@uninorte.edu.co>
2021-06-26 16:24:56 -0500

    Added a Blog Detail comnponent, router and custom useFetch Hook

---
+++ src/BlogDetail.js
@@ -0,0 +1,23 @@
+import { useParams } from "react-router";
+import useFetch from "./useFetch";
+
+const BlogDetail = () => {
+    const { id } = useParams();
+    const { data: blog, error, isPending } = useFetch(`http://localhost:8000/blogs/${id}`);
+
+    return ( 
+        <div className="blog-detail">
+            { error && <div>{ error }</div>}
+            { isPending && <div>Loading...</div>}
+            { blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
+        </div>
+    );
+}
+ 
+export default BlogDetail;
